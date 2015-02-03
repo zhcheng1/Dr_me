276a2dfaf4644c7a077e6998739d0918a1836138
Albert Agram <albert.agram@gmail.com>
2015-02-03 22:57:50 +0000

    added doctor model

--- apps/default/models.py
+++ apps/default/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    SEX_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+        )
+
+    full_name = models.TextField(max_length=100)
+    address = models.TextField(max_length=150)
+    zip_code = models.CharField(max_length=30)
+    age = models.CharField(max=10)
+    sex = models.CharField(max_length=2, choices=SEX_CHOICES, default=FEMALE)
+    specialty = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
