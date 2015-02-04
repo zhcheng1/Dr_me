e16f28c15942124aa24cee22ccebf44aff8b398e
Albert Agram <albert.agram@gmail.com>
2015-02-04 14:28:26 +0000

    added initial views, urls and serializers

--- apps/default/views.py
+++ apps/default/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from apps.default.serializers import *
 
-# Create your views here.
+
+class DoctorList(generics.ListAPIView):
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+
+class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = DoctorSerializer
+    queryset = Doctor.objects.all()
+
+class UserInfoList(generics.ListAPIView):
+    serializer_class = UserInfoSerializer
+    queryset = UserInfo.objects.all()
